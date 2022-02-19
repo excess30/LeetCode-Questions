ee2c483f3ac7ab3c08cd3fd049fe7f445fd325bd
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-02-19 21:54:35 +0530

    Time: 796 ms (5.10%), Space: 12.5 MB (27.23%) - LeetHub

---
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+     int getFrequency(string &s){
+        
+        int cache[26];
+        memset(cache,0,sizeof(cache));
+        
+        char smallest = s[0];
+        for(char ch : s){
+            cache[ch -'a']++;
+            if(smallest > ch)
+                smallest = ch;
+        }
+        return cache[smallest -'a'];
+        
+    }
+    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+          unordered_map<string, int> smallestFreq;
+        vector<int> result;
+        for(string &word : words){
+            smallestFreq[word] = getFrequency(word);
+        }
+        
+        for(string &query : queries){
+            int freq = getFrequency(query);
+            int count = 0;
+            for(string &word : words){
+                if(freq < smallestFreq[word])
+                    count++;
+            }
+            result.push_back(count);
+        }
+        return result;
+    }
+};
