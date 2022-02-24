84c44a5c826c67ecc891f636781dd0103b6bc9e5
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-02-24 21:19:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.35%) - LeetHub

---
+++ 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string strWithout3a3b(int a, int b) 
+    {
+        int A=a;
+        int B=b;
+        std::string res;
+        
+        while (A && B) {
+            if (A > B) {
+                res += "aab";
+                A--;
+            } else if (B > A) {
+                res += "bba";
+                B--;
+            } else {
+                res += "ab";
+            }
+            A--;
+            B--;
+        }
+        while (A--) res += "a";
+        while (B--) res += "b";
+        return res;
+    }
+};
+
+
