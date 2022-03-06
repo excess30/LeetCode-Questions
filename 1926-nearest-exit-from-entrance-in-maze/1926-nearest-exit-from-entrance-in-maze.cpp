94514c6b4820db8023d2543e8d10f8196166aaed
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-03-06 16:03:13 +0530

    Time: 362 ms (6.97%), Space: 79.8 MB (5.11%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    int bfs(vector<vector<char>>& maze,vector<int>& entrance)
+    {
+        int sourceRow=entrance[0];
+        int sourceCol=entrance[1];
+        queue<vector<int>>q;
+        q.push({sourceRow,sourceCol,0});
+        while(q.empty()==false)
+        {
+            auto t = q.front();
+            int x = t[0];
+            int y = t[1];
+            q.pop();
+            if(x<0||x>=maze.size()||y<0||y>=maze[0].size()||maze[x][y]=='+')
+            {
+                continue;
+            }
+            if(x==0||y==0||x==maze.size()-1||y==maze[0].size()-1)
+            {
+                if(maze[x][y]!=maze[sourceRow][sourceCol])
+                {
+                    return t[2];
+                } 
+            }
+                maze[x][y]='+';
+                q.push({x+1,y,t[2]+1});
+                q.push({x-1,y,t[2]+1});
+                q.push({x,y+1,t[2]+1});
+                q.push({x,y-1,t[2]+1});
+            
+        }
+        return -1;
+    }
+    
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) 
+    {
+       return bfs(maze,entrance);
+    }
+};
