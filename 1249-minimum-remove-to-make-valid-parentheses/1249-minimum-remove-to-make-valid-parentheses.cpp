eed85490597cadf6880a9c2e3c12843c7a6ee2fc
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-03-21 17:29:26 +0530

    Time: 28 ms (59.66%), Space: 11.6 MB (51.21%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,56 +2,38 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) 
     {
-         int n = s.length();
-        string res = "";
-
-        int cnt = 0;
-        // iterating from beginnning
-        for (int i = 0; i < s.length(); ++i)
+        stack<int>st;
+        for(int i=0;i<s.size();i++)
         {
-            // if we find and open parenthesis increase the cnt
-            if (s[i] == '(')
-                ++cnt;
-            // if we found an close parenthisis
-            // check if cnt == 0 , that means we have no earlier open parenthesis,
-            // therefore replace that index by '#'
-            else if (s[i] == ')')
+            if(s[i]=='(')
             {
-                if (cnt == 0)
-                    s[i] = '#';
-                // else decrease the cnt beacuse a valid pair is found
+                st.push(i);
+            }
+            else if(s[i]==')')
+            {
+                if(!st.empty())
+                {
+                    st.pop();
+                }
                 else
-                    --cnt;
+                {
+                    s[i]='#';
+                }
             }
         }
-
-        cnt = 0;
-        // iterating from the end
-        for (int i = n - 1; i >= 0; --i)
+        while(!st.empty())
         {
-            // if we find and close parenthesis increase the cnt
-            if (s[i] == ')')
-                ++cnt;
-            // if we found an open parenthisis
-            // check if cnt == 0 , that means we have no earlier close parenthesis,
-            // therefore replace that index by '#'
-            else if (s[i] == '(')
+            s[st.top()]='#';
+            st.pop();
+        }
+        string ans="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]!='#')
             {
-                if (cnt == 0)
-                    s[i] = '#';
-                // else decrease the cnt beacuse a valid pair is found
-                else
-                    --cnt;
+                ans.push_back(s[i]);
             }
         }
-
-        // making the resultant string by excluding '#'
-        for (int i = 0; i < s.length(); ++i)
-        {
-            if (s[i] != '#')
-                res.push_back(s[i]);
-        }
-
-        return res;
+        return ans;
     }
 };
