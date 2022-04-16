631ef450b49da5ca92d551eebf8235df52000bae
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-04-16 15:43:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (85.51%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int minSteps(int n)
+    int minSteps(int n) 
     {
-        if(n<=5)
+        int ans=0;
+        for(int i=2;i*i<=n;)
         {
-            return n==1?0:n;
-        }
-        vector<int>primes={2,3,5,7,11,13,17,19,23,29,31};
-        for(auto x:primes)
-        {
-            if(n%x==0)
+            if(n%i==0)
             {
-                return x+minSteps(n/x);
+                ans+=i;
+                n/=i;
+            }
+            else
+            {
+                i++;
             }
         }
-        return n;
+        if(n!=1)
+        {
+            ans+=n;
+        }
+        return ans;
     }
 };
-
