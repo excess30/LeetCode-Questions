0f0f0ba452d846c3c39f8b7a261e796b5850cae1
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-04-16 20:27:34 +0530

    Time: 25 ms (91.53%), Space: 10.5 MB (48.24%) - LeetHub

--- 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-   vector<int> executeInstructions(int n, vector<int>& start, string s) {
-        int size=s.size();
-        vector<int>ans(size,0);
-        for(int i=0;i<size;i++)
-           ans[i]= findSteps(n,s,start[0],start[1],i);
-        return ans; 
-    }
-    
-    
-    int findSteps(int &n,string &s,int row,int col,int pos){
-       int ans=0;
-        for(int i=pos;i<s.size();i++){
-            if(s[i]=='L')
-                col--;
-            else if(s[i]=='R')
-                col++;
-            else if(s[i]=='U')
-                row--;
-            else
-                row++;
-            if(row<0 || col<0 || row>=n || col>=n)
-                return ans;
-            ans++;
+    vector<int> executeInstructions(int n, vector<int>& startPos, string s) 
+    {
+        vector<int>ans;
+        int j=0;
+        for(int i=0;i<s.size();i++)
+        {
+            j=i;
+            int row = startPos[0];
+            int col = startPos[1];
+            while(j<s.size())
+            {
+                if(s[j]=='U')
+                {
+                    row--;
+                }
+                else if(s[j]=='D')
+                {
+                    row++;
+                }
+                else if(s[j]=='L')
+                {
+                    col--;
+                }
+                else
+                {
+                    col++;
+                }
+                if(row<0||row>=n||col<0||col>=n)
+                {
+                    break;
+                }
+                j++;
+            }
+            ans.push_back(j-i);
         }
         return ans;
     }
 };
+
