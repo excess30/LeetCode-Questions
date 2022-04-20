cd0feacdd555f1270ab6ffce77aac54560c8a0f1
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-04-20 10:46:55 +0530

    Time: 1902 ms (5.00%), Space: 7.5 MB (24.17%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,33 +1,31 @@
 class Solution {
-private:
-    bool doable (const vector<int>& nums, int cuts, long long max) {
-        int acc = 0;
-        for (auto num : nums) {
-            // This step is unnecessary for this problem. I didn't discard this line because I want doable function more generalized.
-            if (num > max) return false;
-            else if (acc + num <= max) acc += num;
-            else {
-                --cuts;
-                acc = num;
-                if (cuts < 0) return false;
-            }
-        }
-        return true;
-    }
-    
 public:
-    int splitArray(vector<int>& nums, int m) {
-        long long left = 0, right = 0;
-        for (auto num : nums) {
-            left = max(left, (long long)num);
-            right += num;
+		int dp[1001][51];     // To memoize it  
+    
+    int solve(vector<int>&nums,int idx, int m){
+        int n = nums.size(); 
+			 // Base case : when the size is equal to 1 i.e. we have to take all the elements. 
+			if (m == 1){      
+            int sum = 0;
+            for(int i = idx; i < n; i++)
+                sum += nums[i]; 
+            return sum; 
         }
+        if (dp[idx][m] != -1)
+            return dp[idx][m]; 
         
-        while (left < right) {
-            long long mid = left + (right - left) / 2;
-            if (doable(nums, m - 1, mid)) right = mid;
-            else left = mid + 1;
+        int ans =   INT_MAX; 
+        int sum = 0; 
+        for(int i = idx; i <= n - m; i++){   // Here n-m because we can't take the elements more then m so that it can be further form non-empty subarray
+            sum += nums[i]; 
+            int next_sum = solve(nums, i+1, m-1);    // Calling for the next maximum sum that can be obtained by spllitting next sequence
+            int temp = max(sum, next_sum);   // Here maximum because we have to consider the largest sum among all the splitted sub array
+            ans = min(ans, temp);  // Answer would be the smallest in all the possible splittings . 
         }
-        return left;
+			return dp[idx][m] = ans; 
+    }
+    int splitArray(vector<int>& nums, int m) {
+        memset(dp, -1, sizeof(dp)); 
+        return solve(nums, 0, m); 
     }
 };
