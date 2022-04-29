79f22443022119cbc1e5b211672a291d84db8a6d
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-04-29 14:58:07 +0530

    Time: 77 ms (56.92%), Space: 21.8 MB (59.15%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<int>>& grid,int i,int j)
+    void search(vector<vector<int>>& grid,int i, int j)
     {
         if(i<0||i>=grid.size()||j<0||j>=grid[0].size()||grid[i][j]==0)
         {
             return;
         }
         grid[i][j]=0;
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        search(grid,i+1,j);
+        search(grid,i-1,j);
+        search(grid,i,j+1);
+        search(grid,i,j-1);
     }
     
     int numEnclaves(vector<vector<int>>& grid) 
     {
         int m = grid.size();
         int n = grid[0].size();
-        int cnt=0;
-        for(int i=0;i<m;i++)
-        {
-            dfs(grid,i,0);
-            dfs(grid,i,n-1);
-        }
         for(int i=0;i<n;i++)
         {
-            dfs(grid,0,i);
-            dfs(grid,m-1,i);
+            search(grid,0,i);
+            search(grid,m-1,i);
         }
+        for(int i=0;i<m;i++)
+        {
+            search(grid,i,0);
+            search(grid,i,n-1);
+        }
+        int cnt=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
