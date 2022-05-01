36c150ebdf575285040e64ca37b501bd067fc638
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-05-01 13:34:09 +0530

    Time: 946 ms (5.00%), Space: 171.8 MB (5.55%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     
-    int bfs(vector<vector<int>>& grid)
+    int bfs(vector<vector<int>>&grid)
     {
         queue<vector<int>>q;
         q.push({0,0,0});
         while(q.empty()==false)
         {
             auto t = q.front();
-            auto x = t[0];
-            auto y = t[1];
+            auto i = t[0];
+            auto j = t[1];
             q.pop();
-            if(x<0||x>=grid.size()||y<0||y>=grid[0].size()||grid[x][y]==1)
+            if(i<0||i>=grid.size()||j<0||j>=grid[0].size()||grid[i][j]==1)
             {
-                continue;
+               continue;
             }
-            if(x==grid.size()-1 && y==grid[0].size()-1)
+            if(i==grid.size()-1 && j==grid[0].size()-1)
             {
                 return t[2]+1;
             }
-            if(grid[x][y]==0)
+            if(grid[i][j]==0)
             {
-                grid[x][y]=1;
-                q.push({x+1,y,t[2]+1});
-                q.push({x-1,y,t[2]+1});
-                q.push({x,y+1,t[2]+1});
-                q.push({x,y-1,t[2]+1});
-                q.push({x-1,y-1,t[2]+1});
-                q.push({x+1,y+1,t[2]+1});
-                q.push({x-1,y+1,t[2]+1});
-                q.push({x+1,y-1,t[2]+1});
+                grid[i][j]=1;
+                q.push({i+1,j,t[2]+1});
+                q.push({i-1,j,t[2]+1});
+                q.push({i,j+1,t[2]+1});
+                q.push({i,j-1,t[2]+1});
+                q.push({i+1,j+1,t[2]+1});
+                q.push({i+1,j-1,t[2]+1});
+                q.push({i-1,j+1,t[2]+1});
+                q.push({i-1,j-1,t[2]+1}); 
             }
         }
         return -1;
     }
-    
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid)
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) 
     {
         int m = grid.size();
         int n = grid[0].size();
@@ -46,5 +45,3 @@ public:
         return bfs(grid);
     }
 };
-
- 
