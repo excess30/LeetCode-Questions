194877c73f04a58e9ff22f9c5b1bb5a87bb4f894
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-05-12 14:30:37 +0530

    Time: 391 ms (5.50%), Space: 79.8 MB (5.02%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -9,27 +9,30 @@ public:
         q.push({sourceRow,sourceCol,0});
         while(q.empty()==false)
         {
-            auto t = q.front();
-            int x = t[0];
-            int y = t[1];
-            q.pop();
-            if(x<0||x>=maze.size()||y<0||y>=maze[0].size()||maze[x][y]=='+')
+            int sz = q.size();
+            while(sz--)
             {
-                continue;
-            }
-            if(x==0||y==0||x==maze.size()-1||y==maze[0].size()-1)
-            {
-                if(maze[x][y]!=maze[sourceRow][sourceCol])
+                auto t = q.front();
+                int x = t[0];
+                int y = t[1];
+                q.pop();
+                if(x<0||x>=maze.size()||y<0||y>=maze[0].size()||maze[x][y]=='+')
                 {
-                    return t[2];
-                } 
+                    continue;
+                }
+                if(x==0||y==0||x==maze.size()-1||y==maze[0].size()-1)
+                {
+                    if(maze[x][y]!=maze[sourceRow][sourceCol])
+                    {
+                        return t[2];
+                    } 
+                }
+                    maze[x][y]='+';
+                    q.push({x+1,y,t[2]+1});
+                    q.push({x-1,y,t[2]+1});
+                    q.push({x,y+1,t[2]+1});
+                    q.push({x,y-1,t[2]+1});
             }
-                maze[x][y]='+';
-                q.push({x+1,y,t[2]+1});
-                q.push({x-1,y,t[2]+1});
-                q.push({x,y+1,t[2]+1});
-                q.push({x,y-1,t[2]+1});
-            
         }
         return -1;
     }
