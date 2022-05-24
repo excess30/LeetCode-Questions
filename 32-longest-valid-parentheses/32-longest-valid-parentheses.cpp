bd5e4002a44d9d71b495a6d54af37ad690b8d0c8
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2022-05-24 14:09:32 +0530

    Time: 4 ms (73.76%), Space: 7.3 MB (67.93%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,6 +1,10 @@
+//My approach : 64/231 passed
+//Push -1 into the stack first and then push the indices of the values which have '(' in them and once yoy encounter a ')' then pop the element from the stack.If popping that element results in the stack being empty push that index in the stack or else find the max length by using i-stk.top().
+
+
 class Solution {
 public:
-    int longestValidParentheses(string s) 
+     int longestValidParentheses(string s) 
     {
         stack<int> stk;
         stk.push(-1);
